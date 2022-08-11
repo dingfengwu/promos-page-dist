dc947b49b4be28f436d0e05643c0f6c794cf8260
devin wu <dingfengwu2008@gmail.com>
2022-08-11 23:02:18 +0800

    保存

--- static/config.js
+++
@@ -1,6 +0,0 @@
-var appConfig = {
-  customerServiceUrl: "https://www.baidu.com",
-  iosAppDownloadUrl: "http://www.google.com",
-  timechatDownloadUrl:"http://www.tc123456.com/index/index/app.html",
-  documentTitle:"金沙国际"
-}
